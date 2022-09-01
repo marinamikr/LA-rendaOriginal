365077238b49547227b6db0b9694816e96cb84e3
Marina <marinahara0223@yahoo.co.jp>
2022-09-02 02:44:56 +0900

    9.2落ちる

--- renda2/ResultViewController.swift
+++ renda2/ResultViewController.swift
@@ -6,29 +6,35 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
 
 class ResultViewController: UIViewController, UITableViewDataSource {
-   
+    
     
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var table: UITableView!
     @IBOutlet var label: UILabel!
     
+    let firestore = Firestore.firestore()
+    var ref: DocumentReference?
+    
     var result: Int = 0
-    var resultArray = [30, 35, 60, 24, 31, 20, 30, 31, 20]
+    //    var resultArray = [30, 35, 60, 24, 31, 20, 30, 31, 20]
+    var resultArray: [String] = []
     let saveDate: UserDefaults = UserDefaults.standard
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        label.text = saveDate.object(forKey: "result") as? String
-//
-//        let value = UserDefaults.standard.array(forKey: "result") as! [String]
-//        print(value)
-//        resultArray = value
-//        saveDate.array(forKey: "resu")
+        //        label.text = saveDate.object(forKey: "result") as? String
+        //
+        //        let value = UserDefaults.standard.array(forKey: "result") as! [String]
+        //        print(value)
+        //        resultArray = value
+        //        saveDate.array(forKey: "resu")
         table.dataSource = self
-
+        
         label.text = String(result)
         
         if result >= 30 {
@@ -36,6 +42,9 @@ class ResultViewController: UIViewController, UITableViewDataSource {
         } else {
             resultLabel.text = "残念..."
         }
+        getCount()
+//        resultArray.sort { $0 > $1 }
+        print(resultArray)
         // Do any additional setup after loading the view.
     }
     
@@ -43,8 +52,8 @@ class ResultViewController: UIViewController, UITableViewDataSource {
         saveDate.set(result, forKey: "result")
         print("保存した")
         self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
-//        self.dismiss(animated: true, completion: nil)
-//        self.presentingViewController?.dismiss(animated: true, completion: nil)
+        //        self.dismiss(animated: true, completion: nil)
+        //        self.presentingViewController?.dismiss(animated: true, completion: nil)
         print("now")
     }
     
@@ -54,8 +63,49 @@ class ResultViewController: UIViewController, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = String(resultArray[indexPath.row])
+        cell?.textLabel?.text = self.resultArray[indexPath.row] as? String
         return cell!
     }
     
+    func getCount() {
+        firestore.collection("count").getDocuments() { (querySnapshot, error) in
+            if let error = error {
+                print("ドキュメントの取得に失敗しました:", error)
+            } else {
+                print("ドキュメントの取得に成功しました")
+                //                for document in querySnapshot!.documents {
+                //                    let data = document.data()
+                //
+                //                    resultArray.append(data)
+                self.resultArray = querySnapshot!.documents.map { document in
+//                    let data = testData(document: document)
+                    let data = document.data()
+                    self.resultArray.sort { $0 > $1 }
+                    return (data as? String)!
+                }
+            }
+            //                    self.resultArray.append(contentsOf: [data])
+        }
+    }
+}
+
+
+//querySnapshotにドキュメントデータが配列になって入っている。
+//                 <ここから先が後編>
+
+
+class testData: NSObject {
+    var uid: String?
+    var data1: String?
+    var data2: Int?
+    
+    
+    //init()について解説置いています。
+    
+    init(document: QueryDocumentSnapshot) {
+        self.uid = document.documentID
+        let Dic = document.data()
+        self.data1 = Dic["data1"] as? String
+        self.data2 = Dic["data2"] as? Int
+    }
 }
---
+++ renda2/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  renda2
+//
+//  Created by 原田摩利奈 on 9/2/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    var testData:testData?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
